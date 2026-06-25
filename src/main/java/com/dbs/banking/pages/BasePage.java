e39d6d3e59d197f8626f9b3ea2406a465686a377
khine282 <khinezarthwe282@gmail.com>
2026-06-25 18:22:18 +0800

    feat: add BasePage with common WebDriver operations (click, type, wait)

---
+++ src/main/java/com/dbs/banking/pages/BasePage.java
@@ -0,0 +1,70 @@
+package com.dbs.banking.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.time.Duration;
+
+public class BasePage {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    private static final Logger logger = LoggerFactory.getLogger(BasePage.class);
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+    }
+
+    public WebElement waitForElement(By locator) {
+        logger.debug("Waiting for element: {}", locator);
+        return wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
+
+    public WebElement waitForClickableElement(By locator) {
+        logger.debug("Waiting for clickable element: {}", locator);
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    public void click(By locator) {
+        logger.debug("Clicking element: {}", locator);
+        waitForClickableElement(locator).click();
+    }
+
+    public void type(By locator, String text) {
+        logger.debug("Typing in element: {}, text: {}", locator, text);
+        WebElement element = waitForElement(locator);
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    public String getText(By locator) {
+        logger.debug("Getting text from element: {}", locator);
+        return waitForElement(locator).getText();
+    }
+
+    public boolean isElementDisplayed(By locator) {
+        try {
+            return waitForElement(locator).isDisplayed();
+        } catch (Exception e) {
+            logger.debug("Element not displayed: {}", locator);
+            return false;
+        }
+    }
+
+    public void navigateTo(String url) {
+        logger.info("Navigating to: {}", url);
+        driver.get(url);
+    }
+
+    public String getCurrentUrl() {
+        return driver.getCurrentUrl();
+    }
+
+    public String getPageTitle() {
+        return driver.getTitle();
+    }
+}
