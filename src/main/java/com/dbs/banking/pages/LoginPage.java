9fca6f894b78a003ceea965c6633fbb60bc3364f
khine282 <khinezarthwe282@gmail.com>
2026-06-25 18:26:42 +0800

    feat: add LoginPage with login workflow and error handling

---
+++ src/main/java/com/dbs/banking/pages/LoginPage.java
@@ -0,0 +1,43 @@
+package com.dbs.banking.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoginPage extends BasePage {
+    private static final Logger logger = LoggerFactory.getLogger(LoginPage.class);
+
+    // Locators
+    private By usernameField = By.id("username");
+    private By passwordField = By.id("password");
+    private By loginButton = By.xpath("//input[@value='Log In']");
+    private By errorMessage = By.className("error");
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void login(String username, String password) {
+        logger.info("Logging in with username: {}", username);
+        type(usernameField, username);
+        type(passwordField, password);
+        click(loginButton);
+        logger.info("Login clicked");
+    }
+
+    public boolean isErrorDisplayed() {
+        return isElementDisplayed(errorMessage);
+    }
+
+    public String getErrorMessage() {
+        if (isErrorDisplayed()) {
+            return getText(errorMessage);
+        }
+        return "";
+    }
+
+    public boolean isLoginPageDisplayed() {
+        return isElementDisplayed(loginButton);
+    }
+}
